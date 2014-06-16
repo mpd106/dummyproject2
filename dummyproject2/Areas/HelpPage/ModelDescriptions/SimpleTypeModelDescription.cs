786a422a2a2f9b9b120310bd9395bae635190fff
unknown <matthew.dickens@dev-matthewd.red-gate.com>
2014-06-16 16:10:49 +0100

    initial commit

---
+++ dummyproject2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace dummyproject2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ dummyproject2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dummyproject2.WebApiApplication" Language="C#" %>
